020880c282c495db5c24bd5a57f7391385f93f74
karchx <kenethra.74@gmail.com>
2026-07-01 05:22:54 -0600

    feat: extern functions

--- src/JIT.hs
+++ src/JIT.hs
@@ -4,6 +4,7 @@ module JIT where
 
 import Data.Word
 import qualified Data.ByteString.Char8 as BS
+import Foreign.Ptr ( FunPtr, castFunPtr )
 
 import Control.Monad.Except
 
@@ -13,8 +14,22 @@ import LLVM.CodeModel
 import LLVM.Module as Mod
 import LLVM.AST
 import qualified LLVM.AST as AST
+import qualified LLVM.ExecutionEngine as EE
 
 import LLVM.PassManager
+
+foreign import ccall "dynamic" haskFun :: FunPtr (IO Double) -> (IO Double)
+
+run :: FunPtr a -> IO Double
+run fn = haskFun (castFunPtr fn :: FunPtr (IO Double))
+
+jit :: Context -> (EE.MCJIT -> IO a) -> IO a
+jit c = EE.withMCJIT c optlevel model ptrelim fastins
+    where
+        optlevel = Just 2
+        model    = Nothing
+        ptrelim  = Nothing
+        fastins  = Nothing
 
 passes :: PassSetSpec
 passes = defaultCuratedPassSetSpec { optLevel = Just 3 }
@@ -22,10 +37,19 @@ passes = defaultCuratedPassSetSpec { optLevel = Just 3 }
 runJIT :: AST.Module -> IO (Either String AST.Module)
 runJIT mod = runExceptT $ do
     liftIO $ withContext $ \context ->
-        withModuleFromAST context mod $ \m -> do
-            withPassManager passes $ \pm -> do
-                runPassManager pm m
-                optmod <- moduleAST m
-                s <- moduleLLVMAssembly m
-                BS.putStrLn s
-                return optmod
+        jit context $ \executionEngine ->
+            withModuleFromAST context mod $ \m -> do
+                withPassManager passes $ \pm -> do
+                    optmod <- moduleAST m
+                    s <- moduleLLVMAssembly m
+                    BS.putStrLn s
+
+                    EE.withModuleInEngine executionEngine m $ \ee -> do
+                        mainfn <- EE.getFunction ee (AST.Name "main")
+                        case mainfn of
+                            Just fn -> do
+                                res <- run fn
+                                putStrLn $ "Evaluated to: " ++ show res
+                            Nothing -> return ()
+
+                    return optmod
